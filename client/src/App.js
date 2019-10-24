01055a9debc9a682f86585d485174830a105e080
Paul David Lampe <pdlampe@gmail.com>
2019-10-23 21:39:38 -0500

    Updated client src code

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,33 @@
-
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Saved from "./pages/Saved";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Wrapper from "./components/Wrapper";
+
+import NoMatch from "./pages/NoMatch";
 import Search from "./pages/Search";
-import Nav from "./components/Nav";
-import { Container } from "./components/Grid";
-import { ToastContainer, Zoom } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import './App.css';
+import Saved from "./pages/Saved";
+
+import "./App.css";
 
 class App extends Component {
+
   render() {
     return (
-      <div>
-        <Nav />
-        <Router>
-          <Container>
-            <ToastContainer
-              position="top-center"
-              autoClose={5000}
-              transition={Zoom}
-              hideProgressBar
-              newestOnTop
-              closeOnClick
-              rtl={false}
-              pauseOnVisibilityChange
-              draggable={false}
-              pauseOnHover
-            />
-            <Switch>
-              <Route exact path="/" component={Search} />
-              <Route path="/bookshelf" component={Saved} />
-              <Route exact path="/books/:id" component={null} />
-              <Route component={null} />
-            </Switch>
-          </Container>
-        </Router>
-      </div>
-    );
-  }
-}
-
+      <Router>
+        <div>
+          <NavBar />
+          <Header />
+          <Wrapper>
+            <Route exact path="/" component={Search} />
+            <Route exact path="/search" component={Search} />
+            <Route exact path="/saved" component={Saved} />
+            <Route exact path="/noMatch" component={NoMatch} />
+          </Wrapper>
+        </div>
+      </Router>
+    )
+  };
+};
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,43 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-
-const mongoose = require('mongoose')
-
-mongoose
-    .connect('mongodb://127.0.0.1:27017/googlebooks', { useNewUrlParser: true })
-    .catch(e => {
-        console.error('Connection error', e.message)
-    })
-
-const db = mongoose.connection
-
-module.exports = db
-
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-
-const db = require('./db')
-
-const app = express()
-const apiPort = 3000
-
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(cors())
-app.use(bodyParser.json())
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
 
 serviceWorker.unregister();
