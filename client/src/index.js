0ba5a225df27f3949ddc571c69c75b3e008c86d4
Paul David Lampe <pdlampe@gmail.com>
2019-10-23 20:57:10 -0500

    Updated routes folder

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,38 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+const mongoose = require('mongoose')
+
+mongoose
+    .connect('mongodb://127.0.0.1:27017/googlebooks', { useNewUrlParser: true })
+    .catch(e => {
+        console.error('Connection error', e.message)
+    })
+
+const db = mongoose.connection
+
+module.exports = db
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const db = require('./db')
+
+const app = express()
+const apiPort = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cors())
+app.use(bodyParser.json())
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(apiPort, () => console.log(`Server running on port ${apiPort}`))
+
 serviceWorker.unregister();
