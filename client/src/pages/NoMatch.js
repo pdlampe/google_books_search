e9a9a6ef8ef349a7c5e53f5f62c4cf7e6b58204c
Paul David Lampe <pdlampe@gmail.com>
2019-10-26 22:14:25 -0500

    No matches code created

---
+++ client/src/pages/NoMatch.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../components/Header";
+
+function noMatch() {
+    return (
+        <div>
+            <Header>
+                <h1>404 Page Not Found</h1>
+
+            </Header>
+        </div>
+    );
+}
+
+export default noMatch;
